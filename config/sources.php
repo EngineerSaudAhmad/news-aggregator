c843e2a0dcba7d4ed154bb4d96bf8ef30e8f92e5
sahmad <engr.saud16@gmail.com>
2024-10-21 23:37:07 +0500

    Implement The Guardian Aggregator and Update README.md and .env.example files

--- config/sources.php
+++ config/sources.php
@@ -4,6 +4,9 @@ return [
     'NewsAPI' => [
         'api_key' => env('NEWSAPI_API_KEY'),
     ],
+    'TheGuardian' => [
+        'api_key' => env('THE_GUARDIAN_API_KEY'),
+    ],
     'NewYorkTimes' => [
         'api_key' => env('NEW_YORK_TIMES_KEY'),
         'api_secret' => env('NEW_YORK_TIMES_SECRET'),
